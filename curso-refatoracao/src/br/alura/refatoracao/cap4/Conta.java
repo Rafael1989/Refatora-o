ee209ab6a5deb1c957a8a30e6a59d4d9e5d71a3b
Intera-PC <Intera-PC@LAPTOP-MPVMO4BN.bs.br.bsch>
2017-10-17 18:33:06 -0200

    Removendo códigos duplicados

---
+++ curso-refatoracao/src/br/alura/refatoracao/cap4/Conta.java
@@ -0,0 +1,28 @@
+package br.alura.refatoracao.cap4;
+
+public class Conta {
+
+	private String titular;
+	private double saldo;
+	
+	public Conta(String titular, double saldo) {
+		this.titular = titular;
+		this.saldo = saldo;
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	public String getTitular() {
+		return titular;
+	}
+	
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
+	
+	public void saca(double valor) {
+		this.saldo -= valor;
+	}
+}
